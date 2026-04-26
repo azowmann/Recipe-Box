4fefc2a3840fb5fe4c28eef6d2a8b3387bde0b34
Alan Zhou <111764623+azowmann@users.noreply.github.com>
2026-04-26 01:02:01 -0400

    Fix test DB isolation and clean schema.rb of Supabase artifacts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.active_record.dump_schema_after_migration = false
 
   # Settings specified here will take precedence over those in config/application.rb.
 
